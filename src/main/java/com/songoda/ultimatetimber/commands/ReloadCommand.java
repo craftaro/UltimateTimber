3a0b05c9875d7198e884084b25725b8edbc025b7
Brianna O'Keefe <bri@songoda.com>
2018-11-23 11:48:07 -0500

    fixed reload for custom items.

--- src/main/java/com/songoda/ultimatetimber/commands/ReloadCommand.java
+++ src/main/java/com/songoda/ultimatetimber/commands/ReloadCommand.java
@@ -1,6 +1,7 @@
 package com.songoda.ultimatetimber.commands;
 
 import com.songoda.ultimatetimber.UltimateTimber;
+import com.songoda.ultimatetimber.treefall.CustomLoot;
 import com.songoda.ultimatetimber.utils.Methods;
 import org.bukkit.command.CommandSender;
 
@@ -9,6 +10,7 @@ public class ReloadCommand {
     public static void reloadConfig(CommandSender commandSender) {
         UltimateTimber plugin = UltimateTimber.getInstance();
         plugin.reloadConfig();
+        CustomLoot.initializeCustomItems();
         commandSender.sendMessage(Methods.formatText(plugin.getPrefix() + " &7Configuration reloaded"));
 
 
