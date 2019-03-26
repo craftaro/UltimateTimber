25a524c230b632d230c24a53a78cc9ab60b6cc74
Esophose <esophose@gmail.com>
2019-03-25 19:44:29 -0600

    Configuration files

--- src/main/java/com/songoda/ultimatetimber/adapter/VersionAdapter.java
+++ src/main/java/com/songoda/ultimatetimber/adapter/VersionAdapter.java
@@ -7,8 +7,22 @@ import java.util.Set;
 
 public interface VersionAdapter {
 
+    /**
+     * Get the items that a block should drop when it breaks
+     *
+     * @param block The target block
+     * @return A Set of ItemStacks that should be dropped
+     */
     Set<ItemStack> getBlockDrops(Block block);
 
+    /**
+     * Applies damage to a tool
+     *
+     * @param blocks The Set of blocks that are being broken
+     * @param tool The tool to apply damage to
+     */
     void applyToolDurability(Set<Block> blocks, ItemStack tool);
+
+
 
 }
