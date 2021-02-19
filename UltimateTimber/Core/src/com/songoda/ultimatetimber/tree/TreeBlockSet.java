4d78edcb453f462cd89050269d156a751983090c
Brianna <bri@songoda.com>
2021-02-19 17:29:12 -0600

    Fixed and modified the way the max tree block setting worked.

--- UltimateTimber/Core/src/com/songoda/ultimatetimber/tree/TreeBlockSet.java
+++ UltimateTimber/Core/src/com/songoda/ultimatetimber/tree/TreeBlockSet.java
@@ -1,26 +1,33 @@
 package com.songoda.ultimatetimber.tree;
 
+import org.bukkit.block.Block;
+
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TreeBlockSet<BlockType> implements Collection {
 
     private final ITreeBlock<BlockType> initialLogBlock;
-    private final Set<ITreeBlock<BlockType>> logBlocks, leafBlocks;
+    private List<ITreeBlock<BlockType>> logBlocks;
+    private final List<ITreeBlock<BlockType>> leafBlocks;
 
     public TreeBlockSet() {
         this.initialLogBlock = null;
-        this.logBlocks = new HashSet<>();
-        this.leafBlocks = new HashSet<>();
+        this.logBlocks = new LinkedList<>();
+        this.leafBlocks = new LinkedList<>();
     }
 
     public TreeBlockSet(ITreeBlock<BlockType> initialLogBlock) {
         this.initialLogBlock = initialLogBlock;
-        this.logBlocks = new HashSet<>();
-        this.leafBlocks = new HashSet<>();
+        this.logBlocks = new LinkedList<>();
+        this.leafBlocks = new LinkedList<>();
 
         if (initialLogBlock != null)
             this.logBlocks.add(initialLogBlock);
@@ -40,8 +47,8 @@ public class TreeBlockSet<BlockType> implements Collection {
      *
      * @return A Set of TreeBlocks
      */
-    public Set<ITreeBlock<BlockType>> getLogBlocks() {
-        return Collections.unmodifiableSet(this.logBlocks);
+    public List<ITreeBlock<BlockType>> getLogBlocks() {
+        return Collections.unmodifiableList(this.logBlocks);
     }
 
     /**
@@ -49,8 +56,8 @@ public class TreeBlockSet<BlockType> implements Collection {
      *
      * @return A Set of TreeBlocks
      */
-    public Set<ITreeBlock<BlockType>> getLeafBlocks() {
-        return Collections.unmodifiableSet(this.leafBlocks);
+    public List<ITreeBlock<BlockType>> getLeafBlocks() {
+        return Collections.unmodifiableList(this.leafBlocks);
     }
 
     /**
@@ -158,6 +165,21 @@ public class TreeBlockSet<BlockType> implements Collection {
             }
         }
         return removedAll;
+    }
+
+    public void sortAndLimit(int max) {
+        if (logBlocks.size() < max)
+            return;
+
+        logBlocks = logBlocks.stream().sorted(Comparator.comparingInt(b -> b.getLocation().getBlockY()))
+                .limit(max).collect(Collectors.toList());
+
+        int highest = logBlocks.get(logBlocks.size() - 1).getLocation().getBlockY();
+
+        if (logBlocks.size() >= max)
+            for (ITreeBlock<BlockType> leafBlock : new LinkedList<>(leafBlocks))
+                if (leafBlock.getLocation().getY() > highest)
+                    leafBlocks.remove(leafBlock);
     }
 
     /**
